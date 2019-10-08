cae71d71944849752ac16d0842c73d4077046816
Kamal-Paramasivam <56102207+Kamal-Paramasivam@users.noreply.github.com>
2019-10-08 21:38:07 +0530

    Update kafka_consumer.py

--- transaction-consumer/kafka_consumer.py
+++ transaction-consumer/kafka_consumer.py
@@ -10,9 +10,9 @@ for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
     print("Topic Subscribed:%s" %message.topic)
-    #print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          #message.offset, message.key,
-                                          #message.value))
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
     redisclient.save_transaction(message.value)
 
 # consume earliest available messages, don't commit offsets
